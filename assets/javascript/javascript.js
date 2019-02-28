e40cc5c2f8e5ca24dc705a3deda4747e902ae5dd
Sam Thompson <electronic62794@yahoo.com>
2019-02-27 21:49:32 -0800

    psudocoded the javascript

---
+++ assets/javascript/javascript.js
@@ -0,0 +1,25 @@
+//create variables that will stand for time left, and score
+
+// create an array of questions
+
+//create a for loop that goes through all the questions
+
+
+//within the for loop if i equals zero, display the possible choices for the answer
+
+//set timer to 30
+
+//create timer function and push into the time-left ID
+
+//create on click events for each of the answers and push into the answer1 answer2 answer3 and answer4 Id's
+
+//if the user clicks the right one, display the Winning screen, if not, display the losing screen. and set time left to 3 seconds.
+
+//imcriment score if the answer was right, do nothing if wrong. 
+
+//else if time reaches 0
+
+// repeat the last 3 steps for i equals 1-4
+
+
+//
