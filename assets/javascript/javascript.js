4f61ec4f27d8ad36ababdf11d8ac1fae02fe1c74
Sam Thompson <electronic62794@yahoo.com>
2019-03-01 19:25:08 -0800

    got everything working except for the score and a consistant timer

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -2,47 +2,53 @@
 var time = 30;
 // var screenTime = 3;
 var score = 0;
-var countDown;
+var interval;
 var i = 0;
 var correct = false;
 // create an array of questions
-var quiz = {
-    question1: "Which of the following is an instrument?",
-    answers1: {
+var quiz = [
+    {
+    question: "Which of the following is an instrument?",
+    answers: {
         right: "Mayonaise",
         wrong: ["Pickles", "Trumpophone", "Horseraddish"]
-    },
-    question2: "On a scale of ice cream sundae, to gluten free pizza crust, how delicious was your meal?",
-    answers2: {
+    }
+},
+{
+    question: "On a scale of ice cream sundae, to gluten free pizza crust, how delicious was your meal?",
+    answers: {
         right: "Yes",
         wrong: ["4 out of 5", "About 15 Brapples", "I'm sorry, that scale makes no sense."]
-    },
-    question3: "Have you ever been so far as to even do look more like?",
-    answers3: {
+    }
+},
+{
+    question: "Have you ever been so far as to even do look more like?",
+    answers: {
         right: "Bruh...I've been even half as many to do more was over yonder.",
         wrong: ["I haven't as more so even done as many yet.", "Even if doing more like somewhat was possible, I wouldn't go that even as to be like.", "Oww....my brain hurt reading this and I think I lost a few brain cells in the process."],
-    },
-    question4: "What is the past tense of Yeet?",
-    answers4: {
+    }},
+    {
+    question: "What is the past tense of Yeet?",
+    answers: {
         right: "Yeeted",
         wrong: ["Yotted", "Yottled", "Yeeten"]
-    },
-    question5: "Which of the following is not one of the main food groups?",
-    answers5: {
+    }},
+    {
+        question: "Which of the following is not one of the main food groups?",
+    answers: {
         right: "Vegetables",
         wrong: ["Candy", "Candy Canes", "Candy Corn"]
     }
-
-}
+    }
+]
 
 
 //create on click events for answer buttons and store them in variables
 
 //create the timer for both 30 and 3 seconds and store them in functions
 
-function createInterval(time) {
-    countDown = setInterval(function timeUp() {
-
+function createInterval(int) {
+    interval = setInterval(function() {
         time--
         $("#time-left").text("Time left: " + time);
         if (time === 0) {
@@ -53,19 +59,24 @@ function createInterval(time) {
 }
 
 function changeQuestion() {
+console.log(this);
 
-    if (correct === true) {
-        i++
-        $("#question").text(questions[i])
-        createInterval(30);
+    if(i=== quiz.length-1){
+        $("#question").text("Your score is " +  score + " out of 4");
     }
+else{
+    if (correct === true) {
+        console.log("going to next question");
+        i++
+        right();   
+     }
     else {
 
         wrong();
 
     }
-
-    clearInterval(countDown);
+}
+    clearInterval(30);
 }
 
 //create a function for the for the losing screen
@@ -77,163 +88,200 @@ function wrong() {
     $("#answer2").text("");
     $("#answer3").text("");
     $("#answer4").text("");
+    clearInterval();
     setTimeout(changeQuestion, 3000);
+    createInterval(3);
 }
 
 //create a function for the correct answer
 
 function right() {
-    // timer();
-    $("#time-left").text(time);
+
     score++;
-    time = 3;
-    $("#question").text("You're a clever one");
-    $("#answer1").text("");
-    $("#answer2").text("");
-    $("#answer3").text("");
-    $("#answer4").text("");
-    clearInterval(countDown);
-    i++
+    $("#question").text(quiz[i].question);
+    $("#answer1").text(quiz[i].answers.right);
+    $("#answer2").text(quiz[i].answers.wrong[0]);
+    $("#answer3").text(quiz[i].answers.wrong[1]);
+    $("#answer4").text(quiz[i].answers.wrong[2]);
+    clearInterval(interval);
+  
 
-}
+};
+$("#answer1").on("click", function(){
+    var answ1 = $(this).text();
+    if(quiz[i].answers.right === answ1){
+        correct = true;
+    }
+    else{correct = false;}
+    console.log(answ1);
+    changeQuestion();
+    
+});
+$("#answer2").on("click", function(){
+    var answ1 = $(this).text();
+    if(quiz[i].answers.right === answ1){
+        correct = true;
+    }
+    else{correct = false;}
+    console.log(answ1);
+    changeQuestion();
+})
+$("#answer3").on("click", function(){
+    var answ1 = $(this).text();
+    if(quiz[i].answers.right === answ1){
+        correct = true;
+    }
+    else{correct = false;}
+    console.log(answ1);
+    changeQuestion();
+})
+$("#answer4").on("click", function(){
+    var answ1 = $(this).text();
+    if(quiz[i].answers.right === answ1){
+        correct = true;
+    }
+    else{correct = false;}
+    console.log(answ1);
+    changeQuestion();
+})
 
 
 
-//create a for loop that goes through all the questions
-// for (i = 0; i < questions.length; i++) {
+createInterval(30)
+$("#question").text(quiz[i].question);
+    $("#answer1").text(quiz[i].answers.right);
+    $("#answer2").text(quiz[i].answers.wrong[0]);
+    $("#answer3").text(quiz[i].answers.wrong[1]);
+    $("#answer4").text(quiz[i].answers.wrong[2]);
 
+    // $("#answer1").on("click", function () {
+    //              right();
+    //      })
+    //          $("#answer2").on("click", function () {
+    //              wrong();
+    //          })
+    //          $("#answer3").on("click", function () {
+    //              wrong();
+    //          })
+    //          $("#answer4").on("click", function () {
+    //              wrong();
+    //          })
 
-// function QnA() {
+    //     if (time === 0) {
+    //         time = 30
+    //         // timer();
+    //         $("$question").text("Gotta be faster than that!");
+    //         i++
+    //     }
+    //     $("#question").text(quiz[0].question);
+    //     $("#answer1").text(quiz[0].answers.wrong[0]);
+    //     $("#answer2").text(quiz[0].answers.wrong[1]);
+    //     $("#answer3").text(quiz[0].answers.right);
+    //     $("#answer4").text(quiz[0].answers.wrong[2]);
+    //     $("#answer1").on("click", function () {
+    //         wrong();
+    //     })
+    //     $("#answer2").on("click", function () {
+    //         wrong();
+    //     })
+    //     $("#answer3").on("click", function () {
+    //         right();
+    //     })
+    //     $("#answer4").on("click", function () {
+    //         wrong();
+    //     })
+  
+
+    // if (i === 1) {
+    //     time = 30;
+    //     // timer();
+    //     $("#question").text(quiz[1].question);
+    //     $("#answer1").text(quiz[1].answers.wrong[0]);
+    //     $("#answer2").text(quyiz[1].answers.right);
+    //     $("#answer3").text(quiz[1].answers.wrong[1]);
+    //     $("#answer4").text(quiz[1].answers.wrong[2]);
+    //     $("#answer1").on("click", function () {
+    //         wrong();
+    //     })
+    //     $("#answer2").on("click", function () {
+    //         right();
+    //     })
+    //     $("#answer3").on("click", function () {
+    //         wrong();
+    //     })
+    //     $("#answer4").on("click", function () {
+    //         wrong();
+    //     })
     // }
-    //within the for loop if i equals zero, display the possible choices for the answer
-    // if (time === 0) {
-    //     time=3
-    // timer();
-    //     $("$question").text("Gotta be faster than that!");
+
+    // if (i === 2) {
+    //     time = 30;
+    //     // timer();
+    //     $("#question").text(quiz[2].question);
+    //     $("#answer1").text(quiz[2].answers.right);
+    //     $("#answer2").text(quiz[2].answers.wrong[0]);
+    //     $("#answer3").text(quiz[2].answers.wrong[1]);
+    //     $("#answer4").text(quiz[2].answers.wrong[2]);
+    //     $("#answer1").on("click", function () {
+    //         right();
+    //     })
+    //     $("#answer2").on("click", function () {
+    //         wrong();
+    //     })
+    //     $("#answer3").on("click", function () {
+    //         wrong();
+    //     })
+    //     $("#answer4").on("click", function () {
+    //         wrong();
+    //     })
     // }
-    if (i === 0) {
-        time = 30;
+    // if (i === 3) {
+    //     time = 30;
+    //     // timer();
+    //     $("#question").text(quiz[3].question);
+    //     $("#answer1").text(quiz[3].answers.wrong[0]);
+    //     $("#answer2").text(quiz[3].answers.right);
+    //     $("#answer3").text(quiz[3].answers.wrong[1]);
+    //     $("#answer4").text(quiz[3].answers.wrong[2]);
+    //     $("#answer1").on("click", function () {
+    //         wrong();
+    //     })
+    //     $("#answer2").on("click", function () {
+    //         right();
+    //     })
+    //     $("#answer3").on("click", function () {
+    //         wrong();
+    //     })
+    //     $("#answer4").on("click", function () {
+    //         wrong();
+    //     })
+    // }
+    // if (i === 4) {
+    //     time = 30;
+    //     // timer();
+    //     $("#question").text(quiz[4].question);
+    //     $("#answer1").text(quiz[4].answers.wrong[0]);
+    //     $("#answer2").text(quiz[4].answers.wrong[1]);
+    //     $("#answer3").text(quiz[4].answers.wrong[2]);
+    //     $("#answer4").text(quiz[4].answers.right);
+    //     $("#answer1").on("click", function () {
+    //         wrong();
+    //     })
+    //     $("#answer2").on("click", function () {
+    //         wrong();
+    //     })
+    //     $("#answer3").on("click", function () {
+    //         wrong();
+    //     })
+    //     $("#answer4").on("click", function () {
+    //         right();
+    //     })
+    // }
 
-        if (time === 0) {
-            time = 3
-            // timer();
-            $("$question").text("Gotta be faster than that!");
-            i++
-        }
-        $("#question").text(quiz.question1);
-        $("#answer1").text(quiz.answers1.wrong[0]);
-        $("#answer2").text(quiz.answers1.wrong[1]);
-        $("#answer3").text(quiz.answers1.right);
-        $("#answer4").text(quiz.answers1.wrong[2]);
-        $("#answer1").on("click", function () {
-            wrong();
-        })
-        $("#answer2").on("click", function () {
-            wrong();
-        })
-        $("#answer3").on("click", function () {
-            right();
-        })
-        $("#answer4").on("click", function () {
-            wrong();
-        })
-    }
+    // if (i === 5) {
+    //     $("#question").text("QUIZ COMPLETE. Your score is: " + score + " out of 5");
+    // }
 
-    if (i === 1) {
-        time = 30;
-        // timer();
-        $("#question").text(quiz.question2);
-        $("#answer1").text(quiz.answers2.wrong[0]);
-        $("#answer2").text(quyiz.answers2.right);
-        $("#answer3").text(quiz.answers2.wrong[1]);
-        $("#answer4").text(quiz.answers2.wrong[2]);
-        $("#answer1").on("click", function () {
-            wrong();
-        })
-        $("#answer2").on("click", function () {
-            right();
-        })
-        $("#answer3").on("click", function () {
-            wrong();
-        })
-        $("#answer4").on("click", function () {
-            wrong();
-        })
-    }
-
-    if (i === 2) {
-        time = 30;
-        // timer();
-        $("#question").text(quiz.question3);
-        $("#answer1").text(quiz.answers3.right);
-        $("#answer2").text(quiz.answers3.wrong[0]);
-        $("#answer3").text(quiz.answers3.wrong[1]);
-        $("#answer4").text(quiz.answers3.wrong[2]);
-        $("#answer1").on("click", function () {
-            right();
-        })
-        $("#answer2").on("click", function () {
-            wrong();
-        })
-        $("#answer3").on("click", function () {
-            wrong();
-        })
-        $("#answer4").on("click", function () {
-            wrong();
-        })
-    }
-    if (i === 3) {
-        time = 30;
-        // timer();
-        $("#question").text(quiz.question4);
-        $("#answer1").text(quiz.answers4.wrong[0]);
-        $("#answer2").text(quiz.answers4.right);
-        $("#answer3").text(quiz.answers4.wrong[1]);
-        $("#answer4").text(quiz.answers4.wrong[2]);
-        $("#answer1").on("click", function () {
-            wrong();
-        })
-        $("#answer2").on("click", function () {
-            right();
-        })
-        $("#answer3").on("click", function () {
-            wrong();
-        })
-        $("#answer4").on("click", function () {
-            wrong();
-        })
-    }
-    if (i === 4) {
-        time = 30;
-        // timer();
-        $("#question").text(quiz.question5);
-        $("#answer1").text(quiz.answers5.wrong[0]);
-        $("#answer2").text(quiz.answers5.wrong[1]);
-        $("#answer3").text(quiz.answers5.wrong[2]);
-        $("#answer4").text(quiz.answers5.right);
-        $("#answer1").on("click", function () {
-            wrong();
-        })
-        $("#answer2").on("click", function () {
-            wrong();
-        })
-        $("#answer3").on("click", function () {
-            wrong();
-        })
-        $("#answer4").on("click", function () {
-            right();
-        })
-    }
-
-    if (i === 5) {
-        $("#question").text("QUIZ COMPLETE. Your score is: " + score + " out of 5");
-    }
-// }
-
-    // i++;
-    // timer(QnA());
-    // i++;
-    // timer(QnA());
         //set timer to 30
 
         //create timer function and push into the time-left ID
